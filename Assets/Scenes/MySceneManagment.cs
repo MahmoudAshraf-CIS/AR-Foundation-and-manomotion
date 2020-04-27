19b06c04e4026dfef473391ddd41b06a785de0c8
developers412 <39314894+developers412@users.noreply.github.com>
2020-04-27 06:46:23 +0200

    Version 0 - template

---
+++ Assets/Scenes/MySceneManagment.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MySceneManagment : MonoBehaviour
+{
+    public int sceneCount = 33;
+    public int sceneIndex;
+    public Text txt;
+
+    private void Awake()
+    {
+        sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        txt.text = SceneManager.GetActiveScene().name;
+    }
+
+    public void next()
+    {
+        SceneManager.LoadScene((sceneIndex + 1) % sceneCount, LoadSceneMode.Single);
+    }
+}
